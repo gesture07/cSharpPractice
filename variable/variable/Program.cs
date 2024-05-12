a82bc7f139a31b6b1e204099d962fd90d3162663
YoonDaHye <1130emma@naver.com>
2024-05-12 15:15:02 +0900

    first commit

---
+++ variable/variable/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "John";
+            Console.WriteLine(name);
+
+            int myNum = 15;
+            Console.WriteLine(myNum);
+
+            myNum = 20;
+            Console.WriteLine(myNum);
+
+            double myDoubleNum = 5.99D;
+            char myLetter = 'D';
+            bool myBool = false;
+            string myText = "Hello";
+            Console.WriteLine(myDoubleNum);
+            Console.WriteLine(myLetter);
+            Console.WriteLine(myBool);
+            Console.WriteLine(myText);
+        }
+    }
+}
